012ad9fdeadfa71514d70355a26ea7f9ddd04c08
KwameElorm <elormh@ymail.com>
2023-10-06 20:18:34 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - this starts the program
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+
+
+
+
+return (0);
+
+}
