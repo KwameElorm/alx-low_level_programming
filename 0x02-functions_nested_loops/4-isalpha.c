d1e2a9efb4c6beb03b80406c0b7e767fb196fe5f
KwameElorm <elormh@ymail.com>
2023-10-10 18:20:11 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *@c:  character to be checked
+ * Return: 0
+ */
+
+int _isalpha(int c)
+
+{
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
+	return (1);
+}
+return (0);
+}
