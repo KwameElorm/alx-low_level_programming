b737805533b304a07af627b2b7efea2dcf2995cb
KwameElorm <elormh@ymail.com>
2023-10-10 17:19:34 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - 10x alphabet
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
