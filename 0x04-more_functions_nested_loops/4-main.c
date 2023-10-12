f671030bdfa218ea96956c6b90a4497e295741c6
KwameElorm <elormh@ymail.com>
2023-10-12 09:06:28 -0700

    questioin3

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-print_most_numbers.c -o 4-print_most_numbers
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef main_h
 #define main_h
-
+#include <unistd.h>
 
 int _isupper(int c);
 int _isdigit(int c);
