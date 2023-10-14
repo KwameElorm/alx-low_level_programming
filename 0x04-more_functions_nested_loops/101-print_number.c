692a30997ce47b85ea3be55a43e2b5591036c26f
KwameElorm <elormh@ymail.com>
2023-10-13 22:43:17 -0700

    deleted

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: input integer parameter
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (1 / 10)
+	{
+		print_numbers(1 / 10);
+	}
+	_putchar(i % 10 + '0');
+}
