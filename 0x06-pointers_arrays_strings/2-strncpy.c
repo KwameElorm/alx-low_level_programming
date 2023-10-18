f03080c0549f0e85aa52292c9ed373206c1053a0
KwameElorm <141396964+KwameElorm@users.noreply.github.com>
2023-10-18 18:54:44 +0000

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncpy- this is the function name
+ *@dest: first parameter
+ *@src: second parameter
+ *@n: third parameter
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	}
+	return (dest);
+}
