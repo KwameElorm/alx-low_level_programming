66a530c04b4055f0c9257eec83fd53698e6eb511
KwameElorm <141396964+KwameElorm@users.noreply.github.com>
2023-10-19 00:39:04 +0000

    2-strncpy.c corrected

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i= 0, src_len = 0;
-	
+	int i = 0, src_len = 0;
+
 	while (src[i++])
 		src_len++;
-	
+
 	for (i = 0; src[i] && i < n; i++)
-		dest[i] = src[i];
+	dest[i] = src[i];
 
 	for (i = src_len; i < n; i++)
-		dest[i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
