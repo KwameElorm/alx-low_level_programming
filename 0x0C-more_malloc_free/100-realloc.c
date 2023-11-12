e7eb27536ba61ea3e03cfa31c04bceed00fedd62
KwameElorm <elormh@ymail.com>
2023-11-12 04:32:57 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,69 +1,29 @@
-#include "main.h"		
+#include "main.h"
 #include <stdlib.h>
-		
 /**
- * argstostr - main entry
- * @ac: int input
- * @av: double pointer array
- * Return: 0
+ * _realloc -  function that reallocates a memory block
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: size, in bytes, of the allocated space of ptr.
+ * @new_size: new size, in bytes, of the new memory block.
+ * Return: ptr.
  */
-		
-char *argstostr(int ac, char **av)
-		
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-		
-	int i, n, r = 0, l = 0;
-		
-	char *str;
-		
-	if (ac == 0 || av == NULL)
-		
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-		for (n = 0; av[i][n]; n++)
-		
-			l++;
-		
 	}
-		
-	l += ac;
-		
-	
-	str = malloc(sizeof(char) * l + 1);
-		
-	if (str == NULL)
-		
-		return (NULL);
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-	for (n = 0; av[i][n]; n++)
-		
-	{
-		
-		str[r] = av[i][n];
-		
-		r++;
-		
-	}
-		
-	if (str[r] == '\0')
-		
-	{
-		
-		str[r++] = '\n';
-		
-	}
-		
-	}
-		
-	return (str);
-		
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
+	free(ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
 
