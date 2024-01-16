bf24b029a7775e27ffdf25c9e7b9871b45340f84
KwameElorm <elormh@ymail.com>
2024-01-15 16:17:02 -0800

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+i#include "lists.h"
+
+/**
+ * free_dlistint - frees dlistint_t list.
+ * @head: pointer to head of the list
+ * Return: 0
+ **/
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+	return;
+
+	while (head->next)
+	{
+	head = head->next;
+	free(head->prev);
+	}
+	free(head);
+}
